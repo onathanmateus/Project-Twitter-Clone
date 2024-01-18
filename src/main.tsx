9f7fefa6c048cba22e3a83bda6b535c9f0ae8291
Nathan Mateus <nathanmateudeo@hotmail.com>
2024-01-18 17:53:28 -0300

    feat: sidebar finished

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,71 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react'
+
+import twitterLogo from './assets/logo-twitter.svg'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <div className="layout">
+      <aside className="sidebar">
+        <img className='logo' src={twitterLogo} alt="logo" />
+
+        <nav className='main-navigation'>
+          <a className='active' href="">
+            <House weight='fill'/>
+            Home
+          </a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            Lists
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+
+        <button className='new-tweet' type='button'></button>
+      </aside>
+      <div className="content">
+        <main className='timeline'>
+          <div className='timeline-header'>
+            Home
+            <Sparkle />
+          </div>
+
+          <form className='new-tweet-form'>
+            <label htmlFor="tweet">
+              <img src="https://github.com/onathanmateus.png" alt="Nathan Mateus" />
+              <textarea id='tweet' placeholder="What's happening ?" />
+            </label>
+
+            <button type='submit'>Tweet</button>
+          </form>
+
+          <div className='separator' />
+        </main>
+      </div>
+    </div>
   </React.StrictMode>,
 )
